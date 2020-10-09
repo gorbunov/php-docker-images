4b23062b8886e554b8cd1854d438432a37cd8b9f
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2020-10-09 12:20:39 +0600

    Basic images in progress, sh script to build and push images until I configure autobuilds on dockerhub.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # php-docker-images
 Clean php docker images
+
+#TODO
+
+- [ ] PHP 8 RC Builds (and stable in the future)
+- [ ] Migrate to multistage builds 
+- [ ] Setup autobuilds on dockerhub
---
+++ build.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+IMAGES=('cli' 'event' 'fpm' 'cli-xdebug' 'fpm-xdebug')
+NAMESPACE="ogorbunov"
+REPO_NAME="php"
+VERSIONS=("7.4" "8.0")
+
+TAGBASE="${NAMESPACE}/${REPO_NAME}"
+
+for VERSION in "${VERSIONS[@]}"; do
+  for TAG in "${IMAGES[@]}"; do
+    TAG_AS="${TAGBASE}:${VERSION}-${TAG}"
+    DOCKERFILE_NAME="./${VERSION}/${TAG}/Dockerfile"
+    if [ -f "$DOCKERFILE_NAME" ]
+    then
+      docker build -t "$TAG_AS" . -f "$DOCKERFILE_NAME"
+    fi
+  done
+done
