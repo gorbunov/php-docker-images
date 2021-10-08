8a46d7e37a076de64e6cc647053fec91482b1988
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2021-10-08 15:10:47 +0600

    8.1 RC3 built, except events

--- build.zsh
+++ build.zsh
@@ -1,9 +1,8 @@
 #!/usr/bin/env zsh
 
 IMAGES=('cli' 'event' 'fpm' 'cli-xdebug' 'event-xdebug' 'fpm-xdebug' 'cli-composer' 'cli-node' 'event-composer' 'fpm-composer')
-VERSIONS=("7.4" "8.0")
-
-IMAGES=('cli-composer' 'cli-node')
+VERSIONS=("7.4" "8.0" "8.1")
+VERSIONS=("8.1")
 
 NAMESPACE="ogorbunov"
 REPO_NAME="php"
